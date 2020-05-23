0c11b87fb4e980e9dd055c9eeaf4d8e85733bf60
OzymandiasB <bominrahmani@gmail.com>
2020-05-23 14:38:56 -0400

    Finished animation and brute force algorithm for Convex Hull

--- ConvexHullAlgo.js
+++ ConvexHullAlgo.js
@@ -4,17 +4,21 @@ const hull = [];
 let left;
 let currentVertex;
 let nextVertex;
+let nextIndex = -1;
 let index = 0;
 
 function setup() {
 createCanvas(500,500);
-  for(let i = 0; i < 10; i++){
-    points.push(createVector(random(width), random(height)));
+  //create buffer to make points stray away from edges of canvas
+  let buffer = 15;
+  for(let i = 0; i < 100; i++){
+    points.push(createVector(random(buffer,width-buffer), random(buffer,height-buffer)));
   }
   //sort the points out first
   points.sort((a,b) => a.x - b.x);
   left = points[0];
   currentVertex = left;
+  hull.push(currentVertex);
   nextVertex = points[1];
   index = 2;
 
@@ -27,9 +31,19 @@ function draw() {
   for(let p of points){
    point(p.x,p.y);
   }
+  stroke(255,0,0);
+  beginShape();
+  fill(255,0,0,50);
+  for(let p of hull){
+    vertex(p.x,p.y)
+  }
+  endShape();
+
+  //animate left most point
   stroke(0,255,0);
-  strokeWeight(24);
+  strokeWeight(12);
   point(left.x,left.y);
+
   //for currentVertex
   stroke(200,255,0);
   strokeWeight(24);
@@ -45,4 +59,27 @@ function draw() {
   stroke(0,0,255);
   line(currentVertex.x,currentVertex.y,checking.x,checking.y);
 
+  //find cross product of vectors
+  const a = p5.Vector.sub(nextVertex,currentVertex);
+  const b = p5.Vector.sub(checking,currentVertex);
+  const cross = a.cross(b);
+  if(cross.z < 0){
+    nextVertex = checking;
+    nextIndex = index;
+  }
+  index++;
+  if(index == points.length){
+    if(nextVertex == left){
+      console.log("done");
+      noLoop();
+    }
+    else{
+    //after all points searched add finalized next vertex into hull
+    hull.push(nextVertex);
+    currentVertex = nextVertex;
+    index = 0;
+    //points.splice(nextIndex,1);
+    nextVertex = left;
+  }
+  }
 }
